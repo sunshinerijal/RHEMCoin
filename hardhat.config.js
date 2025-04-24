be7c19c147f8dd2dc23031a79deffebc09831379
Sunshine <sagarijal@gmail.com>
2025-04-24 20:19:10 +0930

    Update files with Remix IDE changes for RHEMSwap, LiquidityLocker, and dependencies

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,11 @@
+require("@nomiclabs/hardhat-ethers");
 module.exports = {
     solidity: "0.8.20",
     paths: {
         sources: "./contracts",
         tests: "./tests",
         cache: "./cache",
-        artifacts: "./artifacts",
+        artifacts: "./artifacts"
     },
+    sources: ["./contracts", "./interfaces"]
 };
